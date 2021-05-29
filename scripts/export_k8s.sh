b1892bdd91983c366b9e22b980905eda8f0acdba
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2021-05-29 20:39:07 +0100

    Updated deploy script

--- scripts/export_k8s.sh
+++ scripts/export_k8s.sh
@@ -3,3 +3,7 @@ mkdir /workspace || true
 mkdir /workspace/.kube
 echo -n "${_KUBE_CONFIG}"  | base64 -d > /workspace/.kube/config
 echo -n "${_CLIENT_TOKEN}" | base64 -d > /workspace/.kube/token
+chmod 644 /workspace/.kube/config
+
+cat /workspace/.kube/config
+helm upgrade parser parser --install --debug
