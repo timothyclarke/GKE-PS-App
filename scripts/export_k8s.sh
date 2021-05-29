3204427ab995c6d58552a97f93766b5ced1ab0d8
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2021-05-29 20:40:33 +0100

    Updated deploy script

--- scripts/export_k8s.sh
+++ scripts/export_k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -x
 mkdir /workspace || true
-mkdir /workspace/.kube
+mkdir /workspace/.kube || true
 echo -n "${_KUBE_CONFIG}"  | base64 -d > /workspace/.kube/config
 echo -n "${_CLIENT_TOKEN}" | base64 -d > /workspace/.kube/token
 chmod 644 /workspace/.kube/config
